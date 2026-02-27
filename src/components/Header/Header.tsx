75ce62be2dd89413ac82487ccf7c3bdb84cf1969
unknown <fovikson@Proton.me>
2026-02-27 19:04:32 +0100

    add sitemap and fix bug on h1 tag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,8 @@ export default function Header() {
 	return (
 		<header className='h-screen-1/2 w-full px-5 pt-30 dotBackground'>
 			<div className='flex flex-col justify-start items-start m-auto max-w-300 h-full w-full overflow-hidden py-20'>
-				<div>
-					<h1 className='text-black font-fjallaone text-6xl uppercase max-w-[320px] md:text-8xl'>Restauracja Polskie potrawy</h1>
-				</div>
+				<h1 className='text-black font-fjallaone text-6xl uppercase max-w-125 md:text-8xl'>Restauracja Polskie potrawy</h1>
+
 				<SubText />
 				<Toast />
 			</div>
